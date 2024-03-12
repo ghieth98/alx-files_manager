e655597885ec95a42aa964e01d7e2ab5233d2b5a
hossam ghieth <hossamghieth@gmail.com>
2024-03-12 16:41:52 +0200

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectID } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -23,6 +25,25 @@ class UsersController {
           res.status(201).json({ id: user.insertedId, email });
         });
       }
+    });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+    const users = dbClient.db.collection('users');
+    const objectId = new ObjectID(userId);
+    await users.findOne({ _id: objectId }, (result) => {
+      if (!result) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+      }
+      res.status(200).json({ id: userId, email: result.email });
     });
   }
 }
